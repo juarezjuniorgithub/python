af192ba31d0751ec93ea3e35b0a55fc3252ce61f
juarbarb <juarbarb@JUARBARB-IE.ie.oracle.com>
2019-02-17 18:43:29 +0000

    0.0.1

---
+++ python_first_data_types.py
@@ -0,0 +1,8 @@
+# Display something on screen
+
+print("First Data Types")
+
+# It also works with many data types
+
+print(5)
+print(True)
---
+++ python_hello_world.py
@@ -0,0 +1,2 @@
+﻿# this is a comment in Python code
+print ("It works, Juarez Junior!") 
